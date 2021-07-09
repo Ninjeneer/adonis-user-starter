a0db99001ae8b159f8815d382df74ca7212888cc
Loan ALOUACHE <alouache.loan@gmail.com>
2021-07-09 16:16:17 +0200

    Added revoke token endpoint

--- app/Controllers/Http/TokensController.ts
+++ app/Controllers/Http/TokensController.ts
@@ -1,9 +1,18 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
 import ApiToken from "App/Models/ApiToken";
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class TokensController {
 	public async index() {
 		return await ApiToken.all();
+	}
+
+	public async revoke({ request, response }: HttpContextContract) {
+		const token = await ApiToken.findBy('token', request.param('token'));
+		if (token) {
+			token.delete();
+			response.ok({});
+		} else {
+			response.notFound();
+		}
 	}
 }
